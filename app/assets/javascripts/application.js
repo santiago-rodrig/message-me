281ca33a370cf50f20223ccb80829a14c4a92dcc
Santiago <santo1996.29@gmail.com>
2020-01-14 15:46:21 -0400

    jquery rewrited

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,12 +20,7 @@
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
 
-  $('.message .close')
-  .on('click', function() {
-    $(this)
-      .closest('.message')
-      .transition('fade')
-    ;
-  })
-;
+  $('.message .close') .on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 });
