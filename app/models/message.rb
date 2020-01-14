29acd50762c3a756be3f9480ecaa580282c81e7b
Santiago <santo1996.29@gmail.com>
2020-01-14 10:42:34 -0400

    bad naming of foreign key column at messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
   validates :body, presence: true
-  belongs_to :user_id
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@ ActiveRecord::Schema.define(version: 2020_01_14_142143) do
 
   create_table "messages", force: :cascade do |t|
     t.string "body"
-    t.integer "user_id_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id_id"], name: "index_messages_on_user_id_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
