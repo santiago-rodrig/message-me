022c3448158e830f16b312c167858138e45aca91
Santiago <santo1996.29@gmail.com>
2020-01-14 10:33:56 -0400

    message tests for presence

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @message = Message.new(body: 'Testing this model')
+  end
+
+  test 'message body should not be blank' do
+    @message.body = ''
+    assert @message.invalid?
+  end
 end
